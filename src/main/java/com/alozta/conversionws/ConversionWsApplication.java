c4296f533b234a0037de557a84e2228cdc17bb67
Ozan Tacalan <alozta@gmail.com>
2018-12-26 16:32:14 +0300

    Initial commit.

---
+++ src/main/java/com/alozta/conversionws/ConversionWsApplication.java
@@ -0,0 +1,14 @@
+package com.alozta.conversionws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConversionWsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConversionWsApplication.class, args);
+	}
+
+}
+
